61572b9e9d5466d492cfe9708b4ad6e57fad6c8e
张乐 <zhangle_web@163.com>
2019-01-11 16:53:11 +0800

    引入自己的npm组件

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     
-    <Cart />
+    <!-- <Cart /> -->
   </div>
 </template>
 
 <script>
 
+import cellerchanModal from './../node_modules/cellerchan-modal/bundle.js';
 import Cart from './components/Cart'
+
+console.log(cellerchanModal)
 
 export default {
   name: 'app',
   components: {
-    Cart
+    // Cart
   }
 }
 </script>
